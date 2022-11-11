82210200a088e95ee86cb165c6410227c307c365
unknown <kamraneze@code.edu.az>
2022-11-11 21:35:29 +0400

    C# first tasks

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿#region Is this power of 2?
+//string numSt = Console.ReadLine();
+
+//int num = Convert.ToInt32(numSt);
+
+//while (num % 2 == 0)
+//{
+//    num /= 2;
+//}
+//if (num == 1)
+//{
+
+//    Console.WriteLine("This is a power of 2");
+//}
+//else
+//{
+//    Console.WriteLine("No result found");
+
+//} 
+#endregion
+
+
+
+#region 2 eded arasindaki ededler.
+//string randomSt = Console.ReadLine();
+//string randomString = Console.ReadLine();
+
+//int num1 = Convert.ToInt32(randomSt);
+//int num2 = Convert.ToInt32(randomString);
+
+
+//Console.WriteLine("Results: ");
+
+//for (int i = num1; i <= num2; i++)
+//{
+
+//    Console.WriteLine(i);
+//} 
+#endregion
+
+
+
+#region Eded 70-den boyukdur?
+//Console.WriteLine("Eded daxil et:");
+
+//string randSt = Console.ReadLine();
+//int num1 = Convert.ToInt32(randSt);
+
+//if (num1 > 70)
+//{
+//    num1 = 3 * (num1 - 70);
+//    Console.WriteLine("Netice: " + num1);
+//}
+//else 
+//{
+//    num1 -= 70;
+//    Console.WriteLine("Netice: " + num1);
+//} 
+#endregion
+
+
+#region En boyuk eded hansidir?
+//Console.WriteLine("Ededler:");
+
+//string numS1 =Console.ReadLine();
+//string numS2 = Console.ReadLine();
+//string numS3 = Console.ReadLine();
+
+//int num1 = Convert.ToInt32(numS1);
+//int num2 = Convert.ToInt32(numS3);
+//int num3 = Convert.ToInt32(numS2);
+
+//if (num1 > num2 && num1 > num3)
+//{
+//    Console.WriteLine("En boyuk eded: " + num1);
+//}
+//else if (num2 > num1 && num2 > num1)
+//{
+//    Console.WriteLine("En boyuk eded: " + num2);
+//}
+//else
+//{
+//    Console.WriteLine("En boyuk eded: " + num3);
+
+//} 
+#endregion
+
